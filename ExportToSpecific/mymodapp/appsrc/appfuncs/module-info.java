546d230bf6a6b1068f5c0665b73b5aa56aaa0505
benosa <benosa@ROOT>
2022-08-13 02:59:00 +0300

    Add second example

---
+++ ExportToSpecific/mymodapp/appsrc/appfuncs/module-info.java
@@ -0,0 +1,7 @@
+// определение модуля, в котором употребляется
+// предложение to
+module appfuncs {
+    //экспортировать пакет appfuncs.simplefuncs
+    // в модуль appstart
+    exports appfuncs.simplefuncs to appstart;
+}
---
+++ ExportToSpecific/mymodapp/appsrc/appstart/appstart/mymodappdemo/MyModAppDemo.java
@@ -0,0 +1,22 @@
+//продемонстрировать простое модульное приложение
+package appstart.mymodappdemo;
+
+import appfuncs.simplefuncs.SimpleMathFuncs;
+
+public class MyModAppDemo {
+    public static void main(String[] args) {
+        if (SimpleMathFuncs.isFactor(2, 10)) {
+            System.out.println("2 - множитель 10");
+        }
+
+        System.out.println("Наименьший общий множитель "
+                + "35 и 105 - "
+                + SimpleMathFuncs.lcf(35, 105));
+
+        System.out.println("Наибольший общий множитель "
+                + "35 и 105 - "
+                + SimpleMathFuncs.gcf(35, 105));
+    }
+}
+
+
